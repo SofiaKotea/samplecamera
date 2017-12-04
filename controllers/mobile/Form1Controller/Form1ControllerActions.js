5a70d105a86a01720acf36544e7cd80821684401
SofiaKotea <SofiaKotea@users.noreply.github.com>
2017-12-04 11:48:07 +0100

    Initial commit

---
+++ controllers/mobile/Form1Controller/Form1ControllerActions.js
@@ -0,0 +1,12 @@
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    AS_Label_f637b790c3464bfeb69aa96fbad4a7b9: function AS_Label_f637b790c3464bfeb69aa96fbad4a7b9(eventobject, x, y) {
+        var self = this;
+
+        function example4() {
+            kony.application.getCurrentForm().Image0.rawBytes = kony.application.getCurrentForm().Camera.rawBytes;
+        }
+    }
+});
